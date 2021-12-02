7f3e88af85a69df7860ce2872a0c09478ae8f7ea
RussWooDev <79846904+RussWooDev@users.noreply.github.com>
2021-12-02 10:38:16 +0800

    Create dogYears.js

---
+++ dogYears.js
@@ -0,0 +1,17 @@
+//my age
+const myAge = 2;
+
+//early years
+let earlyYears = 2;
+//earlyYears
+earlyYears *= 10.5;
+//later years
+let laterYears = myAge - 2;
+//later years
+laterYears *= 4;
+//later years
+let MyAgeInDogYears = earlyYears + laterYears;
+//later years
+const myName = 'Russ'.toLowerCase();
+//later years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${MyAgeInDogYears} years old in dog years.`); 
